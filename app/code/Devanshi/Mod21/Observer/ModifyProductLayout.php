a363759f28c232307e308d052239e966c03f4da7
devanshi sharma <devanshi.sharma@hbwsl.com>
2025-03-06 15:41:46 +0530

    UserStory 21: Present distinct product detail page layouts

---
+++ app/code/Devanshi/Mod21/Observer/ModifyProductLayout.php
@@ -0,0 +1,27 @@
+<?php
+namespace Devanshi\Mod21\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\App\Request\Http as Request;
+
+class ModifyProductLayout implements ObserverInterface
+{
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function execute(Observer $observer)
+    {
+        $layout = $observer->getEvent()->getLayout();
+        $affiliate = $this->request->getParam('affiliate', false);
+        if ($affiliate) {
+            $layout->getUpdate()->addHandle('catalog_product_view_affiliate');
+        } else {
+            $layout->getUpdate()->addHandle('catalog_product_view');
+        }
+    }
+}
